3f70c15e50ba840b1d4299e06fe3ba80d2d06fa3
Guruprasad <87761603+McQueenix@users.noreply.github.com>
2024-06-06 23:25:24 +0530

    Update main.js

--- Demo/js/main.js
+++ Demo/js/main.js
@@ -1,2 +1,14 @@
-const view1= document.querySelector("#view1");
+const view1= document.getElementById("view1");
 console.log(view1)
+
+const view2= document.querySelector("#view2");
+console.log(view2)
+
+view1.style.display="flex";
+view2.style.display="flex";
+
+const views = document.getElementsByClassName("view");
+console.log(views)
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
